4682f0a8d2e70f49f62cc7f21a724f1d64d22585
Artem <jaman.smlnsk@gmail.com>
2020-04-09 22:02:29 +0300

    2020-04-09 Version 1.1.2: Fixed Nodemailer config and updated README.md

--- src/utils/email.js
+++ src/utils/email.js
@@ -3,7 +3,7 @@ const Nodemailer = require('nodemailer')
 const noReplyTransporter = Nodemailer.createTransport({
   host: process.env.EMAIL_SEVER_HOST,
   port: process.env.EMAIL_SEVER_PORT,
-  secure: process.env.EMAIL_SEVER_SECURE === 1,
+  secure: process.env.EMAIL_SEVER_SECURE === '1',
   auth: {
     user: process.env.EMAIL_SEVER_USER,
     pass: process.env.EMAIL_SEVER_PASSWORD
